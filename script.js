c43756d28c8193aa57f0ea1f7be34a78ca0250c0
Sean Kelly Dausen <146722016+skdausen@users.noreply.github.com>
2023-10-04 14:20:56 +0800

    feat: Activity 2 in DSA
    Comparison operators in javaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+//Activity 2
+let firstFavNum = 10;
+let secondFavNum = 8;
+
+console.log('ACTIVITY 2');
+console.log(firstFavNum > secondFavNum);    // greater than
+console.log(firstFavNum < secondFavNum);    // less than
+console.log(firstFavNum >= secondFavNum);   // greater than or equal
+console.log(firstFavNum <= secondFavNum);   // less than or equal
+console.log(firstFavNum === secondFavNum);   // strict equality
+console.log(firstFavNum == secondFavNum);   // loose equality
+console.log(firstFavNum !== secondFavNum);  // strict inequality
+console.log(firstFavNum != secondFavNum);   // loose inequality
